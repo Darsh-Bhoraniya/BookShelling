6dabad8cdbba69cdc7b9357580f3d832ced44689
Darsh bhoraniya <darshpatel269@gmail.com>
2024-01-06 17:52:22 +0530

    Add project files.

---
+++ BookSheling/Models/Book_Model.cs
@@ -0,0 +1,16 @@
+﻿namespace BookSheling.Models
+{
+    public class Book_Model
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; }
+        public string BookTitle { get; set; }
+        public int BookWiseAuthorID { get; set; }
+        public int TypeID { get; set; }
+        public double Price { get; set; }
+        public int INSBN { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
